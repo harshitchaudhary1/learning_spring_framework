8413476e2149dbcc550915e04697ba480f34e1a7
Harshit Chaudhary <83287475+harshitchaudhary1@users.noreply.github.com>
2024-03-27 02:32:44 +0530

    .

--- learn-spring-framework/src/main/java/com/learningspring/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/learningspring/learnspringframework/AppGamingBasicJava.java
@@ -2,15 +2,16 @@ package com.learningspring.learnspringframework;
 
 import com.learningspring.learnspringframework.game.GameRunner;
 import com.learningspring.learnspringframework.game.MarioGame;
+import com.learningspring.learnspringframework.game.PacmanGame;
 import com.learningspring.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
 
-		var game = new MarioGame();
-	
+//		var game = new MarioGame();
 //		var game = new SuperContraGame();
+		var game = new PacmanGame();
 		var gameRunner = new GameRunner(game);
 		gameRunner.run();
 	}
