3a81f8c41d67c3bf458d09d6e9708bf5892af91d
Harshit Chaudhary <83287475+harshitchaudhary1@users.noreply.github.com>
2024-03-28 01:53:14 +0530

    bean

--- learn-spring-framework/src/main/java/com/learningspring/learnspringframework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/learningspring/learnspringframework/App02HelloWorldSpring.java
@@ -21,7 +21,11 @@ public class App02HelloWorldSpring {
 		
 		System.out.println(context.getBean("person"));
 		
-		System.out.println(context.getBean("address"));
+		System.out.println(context.getBean("person2MethodCall"));
+		
+		System.out.println(context.getBean("Address2"));
+		
+		System.out.println(context.getBean(Address.class));
 	}
 
 }
