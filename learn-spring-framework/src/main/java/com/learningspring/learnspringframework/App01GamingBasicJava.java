1ec505228837b4d43f69deada1e6606206b85e0a
Harshit Chaudhary <83287475+harshitchaudhary1@users.noreply.github.com>
2024-03-27 23:44:25 +0530

    app02

--- learn-spring-framework/src/main/java/com/learningspring/learnspringframework/App01GamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/learningspring/learnspringframework/App01GamingBasicJava.java
@@ -5,7 +5,7 @@ import com.learningspring.learnspringframework.game.MarioGame;
 import com.learningspring.learnspringframework.game.PacmanGame;
 import com.learningspring.learnspringframework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
 
---
+++ learn-spring-framework/src/main/java/com/learningspring/learnspringframework/App02HelloWorldSpring.java
@@ -0,0 +1,10 @@
+package com.learningspring.learnspringframework;
+ 
+public class App02HelloWorldSpring {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
